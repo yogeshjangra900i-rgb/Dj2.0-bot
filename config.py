ca75f17e1d95aaceb1da32dca420f89fac30526d
yogeshjangra900i-rgb <yogeshjangra900i@gmail.com>
2026-04-21 06:47:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+
 class Config(object):
-API_ID    = int(os.environ.get("API_ID", ""))
+  
+API_ID = int(os.environ.get("API_ID", "")) # <— Ye line bilkul chipki hui hai, isliye error hai
 API_HASH  = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
